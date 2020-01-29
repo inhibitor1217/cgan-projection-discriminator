ad8f77b330ef2c1b5c385ae5c2022901c2ddca66
inhibitor1217 <cytops1217@gmail.com>
2020-01-29 17:49:20 +0900

    working for generating single image

--- cifar10_data_loader.py
+++ cifar10_data_loader.py
@@ -15,6 +15,7 @@ DATASET_Y          = 'labels'
 NUM_BATCHES    = len(CIFAR10_DATA_FILES)
 BATCH_SIZE     = 10000
 NUM_TRAIN      = NUM_BATCHES * BATCH_SIZE
+NUM_TRAIN = 1
 IMAGE_SIZE     = (32, 32)
 IMAGE_PIXELS   = IMAGE_SIZE[0] * IMAGE_SIZE[1]
 IMAGE_CHANNELS = 3
@@ -46,8 +47,8 @@ class CIFAR10DataLoader():
                 )
                 labels[np.arange(i*BATCH_SIZE, (i+1)*BATCH_SIZE), labels_batch] = 1.
 
-            h5_file.create_dataset(DATASET_X, data=data)
-            h5_file.create_dataset(DATASET_Y, data=labels)
+            h5_file.create_dataset(DATASET_X, data=data[1:2])
+            h5_file.create_dataset(DATASET_Y, data=labels[1:2])
             h5_file.close()
 
     def generator(self, shuffle=True):
@@ -64,18 +65,23 @@ class CIFAR10DataLoader():
         return _generator(shuffle), NUM_TRAIN
 
     def batch_generator(self, batch_size, shuffle=True, remainder=True):
-        def _generator(shuffle):
-            single_gen, _ = self.generator(shuffle)
-            batch_x = []
-            batch_y = []
-            for x, y in single_gen:
-                batch_x.append(x)
-                batch_y.append(y)
-                if len(batch_x) == batch_size:
-                    yield np.array(batch_x), np.array(batch_y)
-                    del batch_x[:]
-                    del batch_y[:]
-            if remainder and len(batch_x) > 0:
-                yield np.array(batch_x), np.array(batch_y)
         steps = math.ceil(NUM_TRAIN / batch_size) if remainder else (NUM_TRAIN // batch_size)
+        steps = 1
+        def _generator(shuffle):
+            with h5py.File(H5_FILE, 'r+') as h5_file:
+                seed = (id(self) + os.getpid() + int(datetime.now().strftime("%Y%m%d%H%M%S%f"))) % 4294967295
+                np.random.seed(seed)
+                if shuffle:
+                    indices = np.arange(h5_file[DATASET_X].shape[0])
+                    np.random.shuffle(indices)
+                    h5_file[DATASET_X][...] = h5_file[DATASET_X][...][indices]
+                    h5_file[DATASET_Y][...] = h5_file[DATASET_Y][...][indices]
+                for i in range(steps):
+                    if remainder and i == (steps - 1):
+                        yield h5_file[DATASET_X][i*batch_size:], \
+                              h5_file[DATASET_Y][i*batch_size:]
+                    else:
+                        yield h5_file[DATASET_X][i*batch_size:(i+1)*batch_size], \
+                              h5_file[DATASET_Y][i*batch_size:(i+1)*batch_size]
+                h5_file.close()
         return _generator(shuffle), steps
--- main.py
+++ main.py
@@ -1,50 +1,154 @@
 import numpy as np
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Flatten, Dense
+from tensorflow.keras.layers import Concatenate, Reshape, Conv2D, Conv2DTranspose, \
+    BatchNormalization, Dense, GlobalAveragePooling2D, Dot, Add, Lambda, LeakyReLU, Activation
 from tensorflow.keras import Input, Model
 from tensorflow.keras.optimizers import Adam
+import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
-from cifar10_data_loader import CIFAR10DataLoader, IMAGE_SIZE, IMAGE_CHANNELS
+from cifar10_data_loader import CIFAR10DataLoader, NUM_TRAIN, IMAGE_SIZE, IMAGE_CHANNELS, NUM_CLASSES
+from utils import imshow
 
-def define_classifier():
+NOISE_INPUT_SHAPE = (1, 1, 128)
+
+def define_generator():
+    _input = Input(shape=NOISE_INPUT_SHAPE)
+    _label = Input(shape=(NUM_CLASSES,))
+
+    label = Reshape((1, 1, NUM_CLASSES))(_label)
+    x = Concatenate()([_input, label])
+    x = Conv2DTranspose(64, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Conv2DTranspose(64, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Conv2DTranspose(32, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Conv2DTranspose(16, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Conv2DTranspose(16, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Conv2DTranspose(3, 3, 1, activation='tanh', padding='same')(x)
+
+    model = Model([_input, _label], x)
+    # model.summary()
+    return model
+
+def define_discriminator():
     x = _input = Input(shape=(*IMAGE_SIZE, IMAGE_CHANNELS))
+    _label = Input(shape=(NUM_CLASSES,))
 
-    x = Conv2D(16, 4, 2, activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(64, 4, 2, activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(64, 4, 2, activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(64, 4, 2, activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Flatten()(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(10, activation='softmax')(x)
+    x = Conv2D(64, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(64, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(128, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(128, 4, 2, padding='same')(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(NUM_CLASSES, 3, 1, activation='relu', padding='same')(x)
+    x = GlobalAveragePooling2D()(x)
 
-    model = Model(_input, x)
-    model.summary()
+    linear = Dense(1)(x)
+    dot = Dot(axes=-1)([x, _label])
+    x = Add()([linear, dot])
+    x = Activation('sigmoid')(x)
+
+    model = Model([_input, _label], x)
+    # model.summary()
     return model
 
 if __name__ == '__main__':
+
     data_loader = CIFAR10DataLoader()
-    classifier_model = define_classifier()
-    classifier_model.compile(optimizer=Adam(lr=1e-3, beta_1=.9, clipvalue=10., clipnorm=10.),
-                             loss='categorical_crossentropy', metrics=['accuracy'])
 
-    loss_list     = []
-    accuracy_list = []
-    epochs = 30
-    batch_size = 2048
+    g = define_generator()
+    d = define_discriminator()
+
+    def sample_image(num_samples=16):
+        noise = np.random.normal(size=(num_samples * NUM_CLASSES, *NOISE_INPUT_SHAPE))
+        label = np.zeros(shape=(num_samples * NUM_CLASSES, NUM_CLASSES), dtype=np.float32)
+        for i in range(NUM_CLASSES):
+            label[np.arange(i*num_samples, (i+1)*num_samples), i] = 1.
+        sampled = g.predict([noise, label])
+        imshow(sampled, color=True)
+
+    _input_image = Input(shape=(*IMAGE_SIZE, IMAGE_CHANNELS))
+    _input_noise = Input(shape=NOISE_INPUT_SHAPE)
+    _input_label = Input(shape=(NUM_CLASSES,))
+
+    real_prediction = d([_input_image, _input_label])
+    fake_prediction = d([g([_input_noise, _input_label]), _input_label])
+
+    d_trainer_real = Model([_input_image, _input_label], real_prediction)
+    d_trainer_fake = Model([_input_noise, _input_label], fake_prediction)
+    g_trainer      = Model([_input_noise, _input_label], fake_prediction)
+
+    g.trainable = True
+    d.trainable = False
+    g_trainer.compile(optimizer=Adam(lr=1e-4, beta_1=.9, clipvalue=10, clipnorm=10), loss='binary_crossentropy')
+
+    g.trainable = False
+    d.trainable = True
+    d_trainer_real.compile(optimizer=Adam(lr=1e-4, beta_1=.9, clipvalue=10, clipnorm=10), loss='binary_crossentropy')
+    d_trainer_fake.compile(optimizer=Adam(lr=1e-4, beta_1=.9, clipvalue=10, clipnorm=10), loss='binary_crossentropy')
+
+    # g_trainer.summary()
+    # d_trainer_real.summary()
+    # d_trainer_fake.summary()
+
+    d_real_losses = []
+    d_fake_losses = []
+    g_losses = []
+
+    epochs = 2000
+    batch_size = 1024
+    save_freq = 2000
+
     for epoch in range(epochs):
-        data_generator, steps = data_loader.batch_generator(batch_size=batch_size)
-        for step in range(steps):
-            x, y = next(data_generator)
-            [loss, accuracy] = classifier_model.train_on_batch(x, y)
-            print(f'epoch={epoch}, step={step}, loss: {loss}, accuracy: {accuracy}')
-            loss_list.append(loss)
-            accuracy_list.append(accuracy)
+        data_gen, steps = data_loader.batch_generator(batch_size=batch_size)
 
-    plt.figure((16, 16))
-    plt.plot(loss_list)
-    plt.plot(accuracy_list)
-    plt.legend(['loss', 'accuracy'])
+        epoch_d_loss_real = 0.
+        epoch_d_loss_fake = 0.
+        epoch_g_loss      = 0.
+
+        for step in range(steps):
+            image, label = next(data_gen)
+            cur_batch_size = image.shape[0]
+            noise = np.random.normal(0, 1, size=(cur_batch_size, *NOISE_INPUT_SHAPE))
+
+            y_real = np.random.uniform(0.9, 1.0, size=(cur_batch_size, 1))
+            y_fake = np.random.uniform(0.0, 0.1, size=(cur_batch_size, 1))
+
+            d_loss_real = d_trainer_real.train_on_batch([image, label], y_real)
+            d_loss_fake = d_trainer_fake.train_on_batch([noise, label], y_fake)
+            g_loss      = g_trainer.train_on_batch([noise, label], y_real)
+
+            epoch_d_loss_real += d_loss_real * cur_batch_size
+            epoch_d_loss_fake += d_loss_fake * cur_batch_size
+            epoch_g_loss      += g_loss      * cur_batch_size
+
+            print(f'epoch={epoch}, step={step}, d_real={d_loss_real}, d_fake={d_loss_fake}, g_loss={g_loss}')
+
+        epoch_d_loss_real /= NUM_TRAIN
+        epoch_d_loss_fake /= NUM_TRAIN
+        epoch_g_loss      /= NUM_TRAIN
+
+        d_real_losses.append(epoch_d_loss_real)
+        d_fake_losses.append(epoch_d_loss_fake)
+        g_losses.append(epoch_g_loss)
+
+        if (epoch+1) % save_freq == 0:
+            fig = plt.figure(figsize=(16, 16))
+            sample_image()
+            fig.savefig(f'{epoch+1}.png')
+
+    plt.figure(figsize=(8, 8))
+    plt.plot(d_real_losses)
+    plt.plot(d_fake_losses)
+    plt.plot(g_losses)
+    plt.legend(['d_real', 'd_fake', 'g'])
     plt.show()
