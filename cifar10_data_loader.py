256d440b647a3e9f98d9973bf1a77ac1427e65ca
inhibitor1217 <cytops1217@gmail.com>
2020-01-29 10:43:33 +0900

    implement batched data providing

--- cifar10_data_loader.py
+++ cifar10_data_loader.py
@@ -3,9 +3,15 @@ import h5py
 import numpy as np
 from tqdm import tqdm
 from utils import normalize_image
+import os
+from datetime import datetime
+import math
 
 CIFAR10_DATA_PATH  = './cifar-10-python/cifar-10-batches-py/'
 CIFAR10_DATA_FILES = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+H5_FILE            = './cifar10.h5'
+DATASET_X          = 'data'
+DATASET_Y          = 'labels'
 NUM_BATCHES    = len(CIFAR10_DATA_FILES)
 BATCH_SIZE     = 10000
 NUM_TRAIN      = NUM_BATCHES * BATCH_SIZE
@@ -24,7 +30,7 @@ class CIFAR10DataLoader():
         pass
 
     def preprocess(self, files=CIFAR10_DATA_FILES):
-        with h5py.File('cifar10.h5', 'w') as h5_file:
+        with h5py.File(H5_FILE, 'w') as h5_file:
             data   = np.zeros(shape=(NUM_TRAIN, *IMAGE_SIZE, IMAGE_CHANNELS), dtype=np.float32)
             labels = np.zeros(shape=(NUM_TRAIN,), dtype=int)
             for i, file in enumerate(tqdm(files)):
@@ -39,6 +45,36 @@ class CIFAR10DataLoader():
                 )
                 labels[i*BATCH_SIZE:(i+1)*BATCH_SIZE] = labels_batch
 
-            h5_file.create_dataset('data', data=data)
-            h5_file.create_dataset('labels', data=labels)
+            h5_file.create_dataset(DATASET_X, data=data)
+            h5_file.create_dataset(DATASET_Y, data=labels)
             h5_file.close()
+
+    def generator(self, shuffle=True):
+        def _generator(shuffle):
+            with h5py.File(H5_FILE, 'r') as h5_file:
+                seed = (id(self) + os.getpid() + int(datetime.now().strftime("%Y%m%d%H%M%S%f"))) % 4294967295
+                np.random.seed(seed)
+                indices = np.arange(h5_file[DATASET_X].shape[0])
+                if shuffle:
+                    np.random.shuffle(indices)
+                for idx in indices:
+                    yield h5_file[DATASET_X][idx], h5_file[DATASET_Y][idx]
+                h5_file.close()
+        return _generator(shuffle), NUM_TRAIN
+
+    def batch_generator(self, batch_size, shuffle=True, remainder=True):
+        def _generator(shuffle):
+            single_gen, _ = self.generator(shuffle)
+            batch_x = []
+            batch_y = []
+            for x, y in single_gen:
+                batch_x.append(x)
+                batch_y.append(y)
+                if len(batch_x) == batch_size:
+                    yield np.array(batch_x), np.array(batch_y)
+                    del batch_x[:]
+                    del batch_y[:]
+            if remainder and len(batch_x) > 0:
+                yield np.array(batch_x), np.array(batch_y)
+        steps = math.ceil(NUM_TRAIN / batch_size) if remainder else (NUM_TRAIN // batch_size)
+        return _generator(shuffle), steps
